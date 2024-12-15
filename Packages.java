ca152031e4ff461e94e462e5f7b6743c82b552ea
Harsha B <harshab.cs23@bmsce.ac.in>
2024-12-15 17:08:13 +0530

    Create Packages.java

---
+++ Packages.java
@@ -0,0 +1,125 @@
+//CIE/Student
+package CIE;
+import java.util.*;
+
+
+public class Student
+{
+   public String USN;
+   public String Name;
+   public int sem;
+  
+   Scanner scn=new Scanner(System.in);
+
+   public void getd(){
+      System.out.println();
+      scn.nextLine();
+      System.out.println("Enter USN:");
+      USN=scn.nextLine();
+      System.out.println("Enetr Name:");
+      Name=scn.nextLine();
+      System.out.println("Enetr sem:");
+      sem=scn.nextInt();
+   }
+
+   public void putd(){
+     System.out.println("USN:"+USN);
+     System.out.println("Name:"+Name);
+     System.out.println("Sem:"+sem);
+}
+}
+
+//CIE/Internals
+package CIE;
+import java.util.*;
+
+public class Internals
+{
+   Scanner scn=new Scanner(System.in);
+
+   public int Cmarks[]=new int[5];
+  
+   public void getd(){
+     System.out.println("Enter CIE marks:");
+     for(int i=0;i<5;i++){
+       Cmarks[i]=scn.nextInt();
+     }
+   }
+
+  public void putd(){
+     for(int i=0;i<5;i++){
+       System.out.print(Cmarks[i]+" ");
+     }
+   }
+}
+
+//SEE/Externals
+package SEE;
+import CIE.Student;
+import java.util.*;
+
+
+public class External extends Student
+{
+   Scanner scn=new Scanner(System.in);
+
+     public int Smarks[]=new int[5];
+   
+   public void getd(){
+     System.out.println();
+     System.out.println("Enter SEE marks:");
+     for(int i=0;i<5;i++){
+       Smarks[i]=scn.nextInt();
+     }
+   }
+
+   public void putd(){
+      for(int i=0;i<5;i++){
+       System.out.print(Smarks[i]+" ");
+      }
+   }
+
+   public int getFinalMark(int Idx, int internalMark) {
+        return internalMark + (Smarks[Idx] / 2);
+    }
+}
+
+//C_SMain
+import java.util.*;
+import CIE.*; 
+import SEE.*;
+
+class C_SMain
+{
+   public static void main(String[] args){
+   Scanner scn=new Scanner(System.in);
+
+   System.out.println("Enter number of Students:"); 
+   int n=scn.nextInt();  
+ 
+   Student s=new Student();
+
+   Internals I[]=new Internals[n];
+   External E[]=new External[n];
+
+   for(int i=0;i<n;i++){
+       s.getd();
+       s.putd();
+ 
+       I[i]=new Internals();
+       I[i].getd();
+       I[i].putd();
+
+       E[i]=new External();
+       E[i].getd();
+       E[i].putd();
+
+    System.out.println("Final Marks:");
+            for (int j = 0; j < 5; j++) {
+                int finalMark = E[i].getFinalMark(j, I[i].Cmarks[j]);
+                System.out.println(finalMark);
+            }
+    }
+
+     }
+}
